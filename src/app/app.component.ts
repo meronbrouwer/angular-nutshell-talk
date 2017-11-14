cf730d41afd2b8f56798d456d3bd97c57a3d1146
Meron Brouwer <meron.brouwer@gmail.com>
2017-11-14 12:13:28 +0100

    [Step 2] Wired the login fields to the controller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  public onLogin(username: string, password: string): void {
+    console.log('Login has been called, hooray!');
+  }
 }
