d212c0e5199204d934c429e035d1b323a991ba8b
Meron Brouwer <meron.brouwer@gmail.com>
2017-11-14 12:45:48 +0100

    [Step 10] Added a POST request to /login from the login.service

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -1,14 +1,38 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import {LoginResponse} from './dto/login-response.dto';
+import {LoginRequest} from './dto/login-request.dto';
 
 @Injectable()
 export class LoginService {
+
+  private token: string;
+  private user: string;
 
   constructor(private httpClient: HttpClient) {
   }
 
   public login(username: string, password: string): void {
-    console.log('Login is now handled by the Service, more hooray!');
+    const url = 'http://localhost:3000/login';
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    const loginRequest = new LoginRequest(username, password);
+
+    this.httpClient.post<LoginResponse>(url, loginRequest, {headers: headers}).subscribe(
+      data => this.onLoggedIn(data),
+      err => this.onLoginError(err));
   }
 
+  private onLoggedIn(loginResponse: LoginResponse): void {
+    this.token = loginResponse.token;
+    this.user = loginResponse.user;
+
+    console.log('We have received a token! ', this.token);
+  }
+
+  private onLoginError(error: HttpErrorResponse): void {
+    this.token = undefined;
+    this.user = undefined;
+
+    console.log('An error has occured ', error.status);
+  }
 }
