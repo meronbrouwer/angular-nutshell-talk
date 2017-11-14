7bacb19a471b78bd1e141d9b36a06680b6a2870a
Meron Brouwer <meron.brouwer@gmail.com>
2017-11-14 12:53:04 +0100

    [Step 12] Subscribed the root component on the loginService and added dynamic behavior to the view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoginService} from './services/login/login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  public token: string;
 
+  constructor(private loginService: LoginService) {
+  }
+
+  ngOnInit(): void {
+    this.loginService.tokenReceived$.subscribe(token => this.setToken(token));
+  }
+
+  private setToken(token: string): void {
+    this.token = token;
+  }
 }
